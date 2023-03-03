8638c01486338d72dae06ccd7c5506c2f62f48be
deborabcarvalho <121634216+deborabcarvalho@users.noreply.github.com>
2023-03-03 07:56:45 -0300

    Aplicando alteração do tutor
    Apliquei a alteração sugerida pelo tutor, mas o código parou de funcionar.

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
-const form = document.getElementById('form-compara')
+const form = document.getElementById('form-compara');
 
-
-
-
-form.addEventListener("submit", function(e) {
+form.addEventListener('submit', function(e) {
   e.preventDefault();
-
-  const valorA = document.getElementById('valor-A')
-  const valorB = document.getElementById('valor-B')
-  const mensagemSucesso = `O valor B <b>${valorB.value}</b> é maior que o valor A <b>${valorA.value}</b>`
-  const mensagemErro = `O valor B <b>${valorB.value}</b> é MENOR que o valor A <b>${valorA.value}</b>`
   
+  const valorA = document.getElementById('valor-A');
+  const valorB = document.getElementById('valor-B');
   
-  if (valorB.value > valorA.value) {
-    const containerMensageSucess = document.querySelector('.mensagem-sucesso')
-    containerMensageSucess.innerHTML = mensagemSucesso
-    containerMensageSucess.style.display = "block"
-    document.querySelector(".mensagem-erro").style.display = "none"
+  if (validaValor(valorA.valueAsNumber, valorB.valueAsNumber)) {
+    const mensagemSucesso = `O valor B <b>${valorB.value}</b> é maior que o valor A <b>${valorA.value}</b>`;
+    const containerMensageSucess = document.querySelector('.mensagem-sucesso');
+    containerMensageSucess.innerHTML = mensagemSucesso;
+    containerMensageSucess.style.display = "block";
+    document.querySelector(".mensagem-erro").style.display = "none";
 
-    valorA.value = ''
-    valorB.value = ''
-
+    valorA.value = '';
+    valorB.value = '';
   } else {
-    const containerMensageError = document.querySelector('.mensagem-erro')
-    containerMensageError.innerHTML = mensagemErro
-    containerMensageError.style.display = "block"
-    document.querySelector(".mensagem-sucesso").style.display = "none"
+    const mensagemErro = `O valor B <b>${valorB.value}</b> é MENOR que o valor A <b>${valorA.value}</b>`;
+    const containerMensageError = document.querySelector('.mensagem-erro');
+    containerMensageError.innerHTML = mensagemErro;
+    containerMensageError.style.display = "block";
+    document.querySelector(".mensagem-sucesso").style.display = "none";
 
-    valorA.value = ''
-    valorB.value = ''
-
+    valorA.value = '';
+    valorB.value = '';
   }
-
-
-
 });
-
